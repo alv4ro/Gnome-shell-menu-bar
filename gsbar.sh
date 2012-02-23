171725f1ec87773f4472c3d49f8079229f1612cd
Álvaro Pinel <alvaropinel@gmail.com>
2012-02-23 01:54:33 +0100

    added bash-zenity script

---
+++ gsbar.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+REFLEXION="'gnome-shell-reflection@emergya.com'"
+STRING=`gsettings get org.gnome.shell enabled-extensions | sed 's/.\(.*\)/\1/' | sed 's/\(.*\)./\1/' | tr ', ' ' ' ; echo`
+ACT=no
+
+for i in $STRING; do
+    if [ $i = $REFLEXION ]; then
+        ACT=yes
+    else
+        STRING_UP="$STRING_UP,$i"
+    fi
+
+done
+
+NO_REFLEXION=`echo $STRING_UP | sed 's/.\(.*\)/\1/'`
+
+if [ $ACT = "yes" ];then
+    VALUE="[$NO_REFLEXION]"
+    #TODO zenity message to change or cancel
+    if zenity --question --title="Barra de menú de Gnome-shell" --width=350 --text="¿Cambiar la barra de menú a la parte superior de la pantalla?"; then
+        gsettings set org.gnome.shell enabled-extensions "$VALUE"
+        gnome-shell --replace &
+    fi
+
+else
+    VALUE="[$NO_REFLEXION,$REFLEXION]"
+    #TODO zenity message to change or cancel
+    if zenity --question --title="Barra de menú de Gnome-shell" --width=350 --text="¿Cambiar la barra de menú a la \nparte inferior de la pantalla?"; then
+        gsettings set org.gnome.shell enabled-extensions "$VALUE"
+        gnome-shell --replace &
+    fi
+fi
